811c524e95c8b497e060356005456eb615a353c4
Jonespeace <rileydeanjones@gmail.com>
2023-01-31 18:18:34 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+{let numCount = 9
+let count = document.querySelector('.count')
+function addLike(){
+numCount ++
+count.innerText = numCount
+}}
+
+{let numCount = 12
+    let count = document.querySelector('.count-2')
+    function addLikee(){
+    numCount ++
+    count.innerText = numCount
+    }}
+
+    {let numCount = 9
+        let count = document.querySelector('.count-3')
+        function addLikeee(){
+        numCount ++
+        count.innerText = numCount
+        }}
+        
